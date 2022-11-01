d878417d17645be41414cde37e42c6dfbabaf7a8
Fabio Ribeiro <fcmribeiro22@gmail.com>
2022-11-01 23:04:20 +0000

    Add playRound function

--- script.js
+++ script.js
@@ -2,24 +2,47 @@ function getComputerChoice(){
     let compChoice = Math.floor(Math.random() * 3 );
 
         if (compChoice == 0){
-            return "rock";
+            compPlay= "ROck";
         }
         
         else if (compChoice==1){
-            return "scissors";
+            compPlay= "SCIssors";
         }
         
-        return "paper";
+        else{
+            compPlay="PAper";
+        } 
+
+        return compPlay;
     }
 
   
     function playRound(playerSelection, computerSelection) {
-      if (playerSelection.toLowerCase()=="rock" && computerSelection == "scissors"){
-        roundResult= "you win"
+
+      if (playerSelection.toLowerCase() === computerSelection) {
+            roundResult= "It's a draw!";
+      }
+      else if (playerSelection.toLowerCase()=="rock" && computerSelection == "scissors"){
+        roundResult= "You win! Rock beats Scissors";
+      }
+      else if (playerSelection.toLowerCase()=="rock" && computerSelection == "paper"){
+        roundResult= "You lost! Paper beats Rock";
+      }
+      else if (playerSelection.toLowerCase()=="Scissors" && computerSelection == "rock"){
+        roundResult= "You lost! Rock beats Scissors!";
+      }
+      else if (playerSelection.toLowerCase()=="Scissors" && computerSelection == "paper"){
+        roundResult= "You win! Scissors beats Paper";
       }
 
-     
+      else if (playerSelection.toLowerCase()=="Paper" && computerSelection == "rock"){
+        roundResult= "You win! Paper beats Rock!";
+      }
 
+      else if (playerSelection.toLowerCase()=="Paper" && computerSelection == "scissors"){
+        roundResult= "You lost! Scissors beats Paper";
+      }
+     
       return roundResult;
 
         
@@ -27,6 +50,6 @@ function getComputerChoice(){
 
        
 const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
+const computerSelection = getComputerChoice().toLowerCase();
 console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
